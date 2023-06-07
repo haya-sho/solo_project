82bb334486e1cd88a35cc6f5661f24ec49d8c0c7
haya-sho <shoko_hasegawa@mail.toyota.co.jp>
2023-06-07 21:59:30 +0900

    third commit

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -6,10 +6,10 @@ export const List = (props) => {
   const URL =
     process.env.NODE_ENV === "production"
       ? //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
-        "https://bb-master-revenge-front.onrender.com"
+        "https://choose-lunch-app.onrender.com"
       : //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■ここのリンクを変更すること■■■■■■■■■■■■■■■■■■■■■■
         "http://localhost:8080";
-  let patchCheck;
+
   const [menu, menuSet] = useState([]);
 
   const getDataFunc = () => {
@@ -19,18 +19,20 @@ export const List = (props) => {
         menuSet(getData);
       });
   };
+
+  let patchCheck;
   useEffect(() => {
     getDataFunc();
   }, [patchCheck]);
 
-  const DataSort = () => {
-    fetch(`${URL}/table`, { method: "GET" })
-      .then((res) => res.json())
-      .then((getData) => {
-        menuSet(getData);
-      });
-  };
-
+  // const DataSort = () => {
+  //   fetch(`${URL}/table`, { method: "GET" })
+  //     .then((res) => res.json())
+  //     .then((getData) => {
+  //       menuSet(getData);
+  //     });
+  // };
+  console.log(process.env);
   return (
     <>
       <div className="list">
